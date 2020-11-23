afc49b273652a9192d00e09d408a1c60234d0710
407530029 <71250786+407530029@users.noreply.github.com>
2020-11-23 14:58:19 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://407530029.github.io/asdfg/model.json';
 
 
 function preload() {
